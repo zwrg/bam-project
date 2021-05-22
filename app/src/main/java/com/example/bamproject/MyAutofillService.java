b9d3570085db54c75f069eaa477f4bfeee4c10d7
zwrg <riper384@gmail.com>
2021-05-22 13:56:54 +0200

    fix provider security, added no debuggable flag to build

---
+++ app/src/main/java/com/example/bamproject/MyAutofillService.java
@@ -0,0 +1,32 @@
+package com.example.bamproject;
+
+import android.os.Build;
+import android.os.CancellationSignal;
+import android.service.autofill.AutofillService;
+import android.service.autofill.FillCallback;
+import android.service.autofill.FillRequest;
+import android.service.autofill.SaveCallback;
+import android.service.autofill.SaveRequest;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.O)
+public class MyAutofillService extends AutofillService {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+    @Override
+    public void onFillRequest(@NonNull FillRequest request, @NonNull CancellationSignal cancellationSignal, @NonNull FillCallback callback) {
+
+    }
+
+    @Override
+    public void onSaveRequest(@NonNull SaveRequest request, @NonNull SaveCallback callback) {
+
+    }
+}
